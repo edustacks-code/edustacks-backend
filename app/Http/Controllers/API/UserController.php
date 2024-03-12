003e9631695133fc2fca93434e02901bb6a84c42
nationlearns <contact@nationlearns.com>
2024-03-12 20:54:21 +0530

    second commit

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Auth;
+use Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserController extends Controller
+{
+    public function loginUser(Request $request): Response
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+   
+        if($validator->fails()){
+
+            return Response(['message' => $validator->errors()],401);
+        }
+   
+        if(Auth::attempt($request->all())){
+
+            $user = Auth::user(); 
+    
+            $success =  $user->createToken('MyApp')->plainTextToken; 
+        
+            return Response(['token' => $success],200);
+        }
+
+        return Response(['message' => 'email or password wrong'],401);
+    }
+
+    public function createUser (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+        if ($validator->fails())
+        {
+            return response(['errors'=>$validator->errors()->all()], 422);
+        }
+        $request['password']=Hash::make($request['password']);
+        $request['remember_token'] = Str::random(10);
+        $user = User::create($request->toArray());
+        $token = $user->createToken('MyApp')->plainTextToken;
+        $response = ['token' => $token];
+        return response($response, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function userDetails(): Response
+    {
+        if (Auth::check()) {
+
+            $user = Auth::user();
+
+            return Response(['data' => $user],200);
+        }
+
+        return Response(['data' => 'Unauthorized'],401);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function logout(): Response
+    {
+        $user = Auth::user();
+
+        $user->currentAccessToken()->delete();
+        
+        return Response(['data' => 'User Logout successfully.'],200);
+    }
+}
